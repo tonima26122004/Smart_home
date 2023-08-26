c6e21c344135987f3c4d21e15318d0a0b8761def
DON2604 <mrinmaydas2604@gmail.com>
2023-08-27 01:13:24 +0530

    signin signup all fixed

--- app/src/main/java/com/example/openpage/signup.java
+++ app/src/main/java/com/example/openpage/signup.java
@@ -2,7 +2,10 @@ package com.example.openpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class signup extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void openActivity2(View v) {
+        Toast.makeText(getApplicationContext(), "Sign up Page", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, sing_in.class);
+        startActivity(intent);
     }
 }
