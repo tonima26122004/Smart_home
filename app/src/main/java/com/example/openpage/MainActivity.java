6c500ee8b8a9633a59d64a30f377d8842e76e655
DON2604 <mrinmaydas2604@gmail.com>
2023-08-26 23:44:23 +0530

    screen surfing done

--- app/src/main/java/com/example/openpage/MainActivity.java
+++ app/src/main/java/com/example/openpage/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.openpage;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openActivity(View v){
+        Toast.makeText(getApplicationContext(),"Sign in Page",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this , sing_in.class);
+        startActivity(intent);
+
     }
 }
